1bf6a807c7d6422b218e4118edf3802e6fc59bdc
Bill Boing <chengqiangshang@gmail.com>
2026-01-17 13:28:49 +0800

    fix: resolve ESLint unused vars warnings

--- src/collections/Posts/index.ts
+++ src/collections/Posts/index.ts
@@ -10,7 +10,7 @@ import {
 } from '@payloadcms/richtext-lexical'
 
 import { authenticated } from '../../access/authenticated'
-import { authenticatedOrPublished } from '../../access/authenticatedOrPublished'
+// import { authenticatedOrPublished } from '../../access/authenticatedOrPublished'
 import { Banner } from '../../blocks/Banner/config'
 import { Code } from '../../blocks/Code/config'
 import { MediaBlock } from '../../blocks/MediaBlock/config'
