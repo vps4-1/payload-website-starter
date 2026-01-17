94e5a925aed63250bdaf4f4235543b58c434b08d
Bill Boing <chengqiangshang@gmail.com>
2026-01-17 14:10:20 +0800

    fix: add categories to RelatedPosts

--- src/blocks/RelatedPosts/Component.tsx
+++ src/blocks/RelatedPosts/Component.tsx
@@ -1,30 +1,30 @@
-import clsx from 'clsx'
-import React from 'react'
-import RichText from '@/components/RichText'
-
 import type { Post } from '@/payload-types'
 
-import { Card } from '../../components/Card'
-import { DefaultTypedEditorState } from '@payloadcms/richtext-lexical'
+import { Card } from '@/components/Card'
+import React from 'react'
 
 export type RelatedPostsProps = {
   className?: string
   docs?: Post[]
-  introContent?: DefaultTypedEditorState
+  introContent?: any
 }
 
 export const RelatedPosts: React.FC<RelatedPostsProps> = (props) => {
-  const { className, docs, introContent } = props
+  const { docs, introContent } = props
 
   return (
-    <div className={clsx('lg:container', className)}>
-      {introContent && <RichText data={introContent} enableGutter={false} />}
-
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-8 items-stretch">
+    <div className="container">
+      {introContent && <div>{introContent}</div>}
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {docs?.map((doc, index) => {
           if (typeof doc === 'string') return null
 
-          return <Card key={index} doc={doc} relationTo="posts" showCategories />
+          const docWithCategories = {
+            ...doc,
+            categories: []
+          }
+
+          return <Card key={index} doc={docWithCategories} relationTo="posts" showCategories />
         })}
       </div>
     </div>
